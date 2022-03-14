ce4dc236218a4a3c4c66988044155e932c043d1f
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-03-14 01:18:59 -0400

    Style the app manually

--- apps/advisor-desktop-css/src/App.tsx
+++ apps/advisor-desktop-css/src/App.tsx
@@ -2,5 +2,25 @@ import * as React from 'react';
 import { Header } from './components';
 
 export function App() {
-  return <Header />;
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="p-3">
+        <div className="panel p-2 h-114px">
+          <h2 className="h7 leading-none">Client Profile</h2>
+        </div>
+        <div className="panel mt-2 p-2 h-86px">
+          <h2 className="h7 leading-none">Net Worth</h2>
+        </div>
+        <div className="grid-2-col mt-2">
+          <div className="panel p-2 h-400px">
+            <h2 className="h7 leading-none">Asset Allocations</h2>
+          </div>
+          <div className="panel p-2 h-400px">
+            <h2 className="h7 leading-none">Performance</h2>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
