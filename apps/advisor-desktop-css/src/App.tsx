7864a088512a904159ef287d719c01fa84d46882
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-03-15 01:12:23 -0400

    Changed "Panel" to "Card" in code

--- apps/advisor-desktop-css/src/App.tsx
+++ apps/advisor-desktop-css/src/App.tsx
@@ -6,17 +6,17 @@ export function App() {
     <React.Fragment>
       <Header />
       <div className="p-3">
-        <div className="panel p-2 h-114px">
+        <div className="card p-2 h-114px">
           <h2 className="h7 leading-none">Client Profile</h2>
         </div>
-        <div className="panel mt-2 p-2 h-86px">
+        <div className="card mt-2 p-2 h-86px">
           <h2 className="h7 leading-none">Net Worth</h2>
         </div>
         <div className="grid-2-col mt-2">
-          <div className="panel p-2 h-400px">
+          <div className="card p-2 h-400px">
             <h2 className="h7 leading-none">Asset Allocations</h2>
           </div>
-          <div className="panel p-2 h-400px">
+          <div className="card p-2 h-400px">
             <h2 className="h7 leading-none">Performance</h2>
           </div>
         </div>
