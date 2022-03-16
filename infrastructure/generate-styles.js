faf067d7905bcf072533c9cab59328f71e08cf95
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-03-15 20:38:27 -0400

    Refactored generate-styles.js

--- infrastructure/generate-styles.js
+++ infrastructure/generate-styles.js
@@ -1,10 +1,12 @@
-// ***** Generates styles using Style Dictionary *****
+/**
+ * Generates styles using Style Dictionary
+ */
 
-const StyleDictionaryPackage = require('style-dictionary');
+const StyleDictionary = require('style-dictionary');
 const { createArray } = require('./fns');
 
 /** Generates style dictionary config dynamically */
-StyleDictionaryPackage.registerFormat({
+StyleDictionary.registerFormat({
   name: 'css/variables',
   formatter: function (dictionary, config) {
     return `${this.selector} {\n${dictionary.allProperties
@@ -13,25 +15,61 @@ StyleDictionaryPackage.registerFormat({
   },
 });
 
-/** Registers required transformations */
-StyleDictionaryPackage.registerTransform({
+/** Registers transform for sizes */
+StyleDictionary.registerTransform({
   name: 'sizes/px',
   type: 'value',
   matcher: function (prop) {
     // You can be more specific here if you only want 'em' units for font sizes
+    // TODO: Rethink how sizes should be transformed
     return [
-      'fontSizes',
-      'spacing',
-      'borderRadius',
-      'borderWidth',
-      'sizing',
+      // 'fontSize',
+      // 'spacing',
+      // 'borderRadius',
+      // 'borderWidth',
+      // 'sizing',
     ].includes(prop.attributes.category);
   },
   transformer: function (prop) {
-    // You can also modify the value here if you want to convert pixels to ems
     return parseFloat(prop.original.value) + 'px';
   },
 });
+
+/** Registers transform for boxShadow */
+StyleDictionary.registerTransform({
+  name: 'shadow/spreadShadow',
+  type: 'value',
+  matcher: function (token) {
+    return token.type === 'boxShadow';
+  },
+  transformer: (token) => {
+    const shadows = Object.values(token.value);
+    const result = shadows.map(
+      (shadow) =>
+        `${shadow.x} ${shadow.y} ${shadow.blur} ${shadow.spread} ${shadow.color}`
+    );
+    return result.join(',');
+  },
+});
+
+console.log('Build started...');
+
+/** Processes global tokens and theme specific tokens */
+['global', 'blue-light', 'blue-dark', 'grey-light', 'grey-dark'].map(function (
+  theme
+) {
+  console.log(`\nProcessing: [${theme}]`);
+
+  const StyleDictionaryExtended = StyleDictionary.extend(getThemeConfig(theme));
+
+  StyleDictionaryExtended.buildPlatform('web');
+
+  console.log('==============================================');
+});
+
+console.log('\nBuild completed!');
+
+// ---------- Helper functions ----------
 
 /** Returns the configuration for a theme */
 function getThemeConfig(theme) {
@@ -42,7 +80,12 @@ function getThemeConfig(theme) {
     },
     platforms: {
       web: {
-        transforms: ['attribute/cti', 'name/cti/kebab', 'sizes/px'],
+        transforms: [
+          'attribute/cti',
+          'name/cti/kebab',
+          'sizes/px',
+          'shadow/spreadShadow',
+        ],
         buildPath: 'apps/advisor-desktop-css/src/styles/output/',
         files: [
           {
@@ -59,28 +102,3 @@ function getThemeConfig(theme) {
     },
   };
 }
-
-/** Processes global tokens and theme specific tokens */
-function main() {
-  console.log('Build started...');
-
-  ['global', 'blue-light', 'blue-dark', 'grey-light', 'grey-dark'].map(
-    function (theme) {
-      console.log('\n==============================================');
-      console.log(`\nProcessing: [${theme}]`);
-
-      const StyleDictionary = StyleDictionaryPackage.extend(
-        getThemeConfig(theme)
-      );
-
-      StyleDictionary.buildPlatform('web');
-
-      console.log('\nEnd processing');
-    }
-  );
-
-  console.log('\n==============================================');
-  console.log('\nBuild completed!');
-}
-
-main();
