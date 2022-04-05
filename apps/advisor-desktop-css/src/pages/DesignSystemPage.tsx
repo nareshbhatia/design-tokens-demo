2dfb9489d9dc2c7189d71ee3c057a659280360ca
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-04-05 19:38:30 -0400

    Fixed documentation errors on DesignSystemPage

--- apps/advisor-desktop-css/src/pages/DesignSystemPage.tsx
+++ apps/advisor-desktop-css/src/pages/DesignSystemPage.tsx
@@ -33,13 +33,13 @@ export function DesignSystemPage() {
       <div className="p-3">
         <div className="card max-w-800 p-3">
           <h1 className="h1">h1. Heading 1</h1>
-          <h2 className="h2 mt-1">h2. Heading 1</h2>
-          <h3 className="h3 mt-1">h3. Heading 1</h3>
-          <h4 className="h4 mt-1">h4. Heading 1</h4>
-          <h5 className="h5 mt-1">h5. Heading 1</h5>
-          <h6 className="h6 mt-1">h6. Heading 1</h6>
-          <h6 className="h6sm mt-1">h6sm. Heading 1</h6>
-          <h6 className="h6xs mt-1">h6xs. Heading 1</h6>
+          <h2 className="h2 mt-1">h2. Heading 2</h2>
+          <h3 className="h3 mt-1">h3. Heading 3</h3>
+          <h4 className="h4 mt-1">h4. Heading 4</h4>
+          <h5 className="h5 mt-1">h5. Heading 5</h5>
+          <h6 className="h6 mt-1">h6. Heading 6</h6>
+          <h6 className="h6sm mt-1">h6sm. Heading 6sm</h6>
+          <h6 className="h6xs mt-1">h6xs. Heading 6xs</h6>
           <p className="body1 mt-1">
             body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore
